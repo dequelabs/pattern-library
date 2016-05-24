9430e562b5edbec46ddb5f197f92a76af18ad134
Harris Schneiderman <harris.schneiderman@deque.com>
2016-05-24 13:50:33 -0400

    Gets the animationz right

--- playground/sso/login.js
+++ playground/sso/login.js
@@ -1,51 +1,107 @@
 (function () {
   'use strict';
 
-  jQuery('.step-1 button[type="submit"]').on('click', function (e) {
+  // sections
+  var $mainLoginEmail = jQuery('#login-email-section');
+  var $mainLoginPassword = jQuery('#login-password-section');
+  var $forgotPassword = jQuery('#forgot-password-section');
+  var $forgotPasswordSent = jQuery('#forgot-password-sent-section');
+  var $resetPassword = jQuery('#reset-password-section');
+  var $twoFactor = jQuery('#two-factor-section');
+  var $email = jQuery('#email');
+
+  $mainLoginEmail.find('button[type="submit"]').on('click', function (e) {
     e.preventDefault(); // don't submit just yet...
-    // TODO: validation
-    step2();
+    flyIn({
+      animate: 'right',
+      $hide: $mainLoginEmail,
+      $show: $mainLoginPassword,
+      fillIn: {
+        text: $email.val(),
+        $target: jQuery('#user-email-display, #user-email')
+      },
+      $focus: jQuery('#password')
+    });
   });
 
-  jQuery('#forgot-password button[type="submit"]').on('click', function (e) {
-    e.preventDefault(); // don't submit just yet...
-    slideIn(jQuery('#forgot-password'), jQuery('#forgot-password-sent'), jQuery('#forgot-password-sent'));
-    var email = jQuery('#forgot-password-email').val();
-    jQuery('#email-sent-to').text(email);
+  $mainLoginPassword.find('.forgot-password').on('click', function () {
+    var $input = jQuery('#forgot-password-email');
+    flyIn({
+      animate: 'fade',
+      $hide: $mainLoginPassword,
+      $show: $forgotPassword,
+      fillIn: {
+        text: $email.val(),
+        $target: $input
+      },
+      $focus: $input
+    });
   });
 
-  jQuery('.different-email, .back-to-sign-in').on('click', step1);
-
-  jQuery('.forgot-password').on('click', function () {
-    slideIn(jQuery('.step-1, .step-2'), jQuery('#forgot-password'), jQuery('#forgot-password-email'));
-    jQuery('#forgot-password-email').val(jQuery('#email').val());
+  $mainLoginPassword.find('.different-email').on('click', function () {
+    console.log('yoep~!');
+    flyIn({
+      animate: 'left',
+      $hide: $mainLoginPassword,
+      $show: $mainLoginEmail,
+      $focus: $email
+    });
   });
 
-  function step2() {
-    var emailVal = jQuery('#email').val();
-    jQuery('#user-email-display').text(emailVal);
-    jQuery('#user-email').val(emailVal);
-    slideIn(jQuery('.step-1, #forgot-password'), jQuery('.step-2'), jQuery('#password'));
-  }
+  $forgotPassword.find('.back-to-sign-in').on('click', function () {
+    flyIn({
+      animate: 'left',
+      $hide: $forgotPassword,
+      $show: $mainLoginEmail,
+      $focus: $email
+    });
+  });
 
-  function step1() {
-    slideIn(
-      jQuery('#forgot-password, .step-2, #two-factor, #reset-password-wrap'),
-      jQuery('.step-1'),
-      jQuery('#email')
-    );
-  }
-
-  function slideIn($hiders, $animater, $focus) {
-    $hiders.hide();
-    $hiders.removeClass('tile-show animate');
-    
-    $animater.show().addClass('tile-show');
-    setTimeout(function () {
-      $animater.addClass('animate');
-      if ($focus) {
-        $focus.focus();
+  $forgotPassword.find('button[type="submit"]').on('click', function (e) {
+    e.preventDefault();
+    flyIn({
+      animate: 'fade',
+      $hide: $forgotPassword,
+      $show: $forgotPasswordSent,
+      $focus: $forgotPasswordSent,
+      fillIn: {
+        text: jQuery('#forgot-password-email').val(),
+        $target: jQuery('#email-sent-to')
       }
     });
+  });
+
+  /**
+   * Handles toggling different sections
+   * @param  {Object} opts $hide and $show are required. The rest are optional (animate, fillIn and $focus)
+   */
+  function flyIn(opts) {
+    // hide
+    opts.$hide.removeClass('active setup left right fade');
+
+    // fill in value(s)
+    if (opts.fillIn) {
+      opts.fillIn.$target.each(function (_, el) {
+        var $el = jQuery(el);
+        if ($el.is('input')) {
+          $el.val(opts.fillIn.text);
+        } else {
+          $el.text(opts.fillIn.text);
+        }
+      });
+    }
+
+    // show
+    var $show = opts.$show;
+    var animation = opts.animate || 'fade';
+    $show.addClass('setup ' + animation);
+    $show.addClass('active');
+    setTimeout(function () {
+      $show.removeClass(animation + ' setup');
+      // focus
+      if (opts.$focus) {
+        opts.$focus.focus();
+      }
+    }, 10);
   }
 }());
